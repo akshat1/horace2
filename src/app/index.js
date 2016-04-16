62c6089947792dd0e4848d20506d0745e98614f9
akshat1 <akshat.sh@gmail.com>
2016-04-15 21:16:33 -0700

    - Moved socket.io stuff into separate file. - moved api routes into separate file

--- src/app/index.js
+++ src/app/index.js
@@ -3,30 +3,29 @@
 /**
  * @module index
  */
-
-var Path = require('path');
-var Winston = require('winston');
-var Express = require('express');
-var BodyParser = require('body-parser');
-var ServeStatic = require('serve-static');
-var SocketIO = require('socket.io');
-var FSExtra = require('fs-extra');
-var _ = require('lodash');
-var Mime = require('mime');
-
-var ServerEvents = require('./events.js').Server;
-var UrlMap = require('./urls.js').UrlMap;
-var Config = require('./config.js');
-var Horace = require('./horace.js');
-
+const Path         = require('path');
+const Winston      = require('winston');
+const Express      = require('express');
+const BodyParser   = require('body-parser');
+const ServeStatic  = require('serve-static');
+const FSExtra      = require('fs-extra');
+const _            = require('lodash');
+const Mime         = require('mime');
+const UrlMap       = require('./urls.js').UrlMap;
 const ServerUrlMap = UrlMap.Server;
+const Config       = require('./config.js');
+const Horace       = require('./horace.js');
+const websockets   = require('./server/websockets.js');
+const apiRouter    = require('./server/apiRouter.js');
 
+
+FSExtra.ensureDir('log');
 const logger = new Winston.Logger({
   transports: [
     new Winston.transports.Console({
       level: logLevel
     }), new Winston.transports.File({
-      filename: 'horace-server.log'
+      filename: 'log/horace-server.log'
     })
   ]
 });
@@ -40,16 +39,16 @@ if (Config('horace.rebuildClientAtStartup')) {
 
 
 // Start Server
-const logLevel       = Config('horace.server.logLevel');
-const serverTmpPath  = Path.join(__dirname, '..', Config('horace.tmpDirPath'));
-const serverSubDir   = Config('horace.urlSubDir').replace(/\/$/, '') || '/';
-const webroot        = Path.join(__dirname, '..', Config('horace.webroot'));
-const listenPort     = new Number(Config('horace.port')).valueOf();
-const urlRoot        = serverSubDir;
-const socketIOURL    = Path.join('/', serverSubDir, 'socket.io');
-const app = Express();
+const logLevel      = Config('horace.server.logLevel');
+const serverTmpPath = Path.join(__dirname, '..', Config('horace.tmpDirPath'));
+const serverSubDir  = Config('horace.urlSubDir').replace(/\/$/, '') || '/';
+const webroot       = Path.join(__dirname, '..', Config('horace.webroot'));
+const socketIOURL = Path.join('/', serverSubDir, 'socket.io');
+const listenPort    = new Number(Config('horace.port')).valueOf();
+const urlRoot       = serverSubDir;
+const app           = Express();
+
 app.use(BodyParser.json());
-const apiRouter = Express.Router();
 
 logger.info('listenPort    : ', listenPort);
 logger.info('serverTmpPath : ', serverTmpPath);
@@ -72,44 +71,7 @@ const server = app.listen(listenPort, function() {
 });
 
 //Set up websockets
-const io = SocketIO.listen(server, {
-  path: socketIOURL
-});
-
-
-//Websocket broadcasts
-Horace.on(ServerEvents.SCANNER_SCANSTARTED, function() {
-  io.emit(ServerEvents.SCANNER_SCANSTARTED);
-});
-
-
-Horace.on(ServerEvents.SCANNER_SCANSTOPPED, function() {
-  io.emit(ServerEvents.SCANNER_SCANSTOPPED);
-});
-
-
-io.on('connection', function(socket) {
-  console.log('WS Connected!'); //TODO: Find out why this is printed twice
-  socket.emit('hello', {
-    id: socket.id
-  });
-
-  socket.on(ServerEvents.REQUEST_BOOK_DOWNLOAD, function(query) {
-    logger.debug('BOOK DOWNLOAD REQUESTED // %o', query);
-    return Horace.requestDownload(parseInt(query.bookId))
-      .then(function(tmpFilePath) {
-        logger.debug(`\n$$$$$\nsend message that the download is ready at ${tmpFilePath}.`);
-        let fileName = Path.basename(tmpFilePath);
-        return socket.emit(ServerEvents.BOOK_READY_FOR_DOWNLOAD, {
-          path: ServerUrlMap.fileDownload(fileName)
-        });
-      })
-      .catch(function(err) {
-        return console.error('\n$$$$$\nsend error via socket', err);
-      });
-  });
-});
-
+websockets.setUp(socketIOURL, server, Horace);
 
 // Set up routes
 appUse(ServerUrlMap.fileDownload(), function (request, response) {
@@ -139,78 +101,5 @@ appUse(ServerUrlMap.Config, function(req, res) {
 });
 
 
-// Set up the api router
-apiRouter.get(ServerUrlMap['Command.StartScan'], function(request, response) {
-  logger.debug('Start Scan');
-  Horace.startScan();
-  return response.send('OK');
-});
 
-
-apiRouter.get(ServerUrlMap['Status.IsScanning'], function(request, response) {
-  return response.json(Horace.isScanningForBooks());
-});
-
-
-apiRouter.post(ServerUrlMap['Books'], function(request, response) {
-  logger.debug('getBooks');
-  var query = request.body;
-  return Horace.getBooks(query)
-    .then(function(res) {
-      return response.json(res);
-    })
-    .catch(function(err) {
-      logger.error('Error fetching books from Horace %o', err);
-      return response.status(500).send(err);
-    });
-});
-
-
-apiRouter.get(ServerUrlMap['Books.Distinct'], function(request, response) {
-  logger.debug(`get[${ServerUrlMap['Books.Distinct']}]`);
-  var columnName = request.params.columnName;
-  Horace.getDistinctBookAttribute(columnName)
-    .then(function(values){
-      logger.debug(`Got ${values.length} distinct values`);
-      response.json(values);
-      return;
-    })
-    .catch(function(err){
-      logger.error(`Error fetching distinct value for book['${columnName}']`, err);
-      response.status(500).send(err);
-      return;
-    });
-  return;
-});
-
-
-apiRouter.get(ServerUrlMap['Book.Hide'], function(request, response) {
-  logger.debug(`get[${ServerUrlMap['BookHide']}]`);
-  let bookIds = request.params.bookIds;
-  console.log('hide bookIds: ', bookIds);
-  console.log(typeof bookIds);
-  bookIds = bookIds.split(',');
-  console.log(typeof bookIds);
-  Promise.all(bookIds.map(function(bookId){
-    return Horace.hideBook(bookId);
-  }))
-    .then(function() {
-      console.log('ALL BOOKS HIDDEN');
-      return response.status(200).send('OK');
-    })
-    .catch(function(err) {
-      logger.error(`Error hiding book ${bookId}`, err);
-      response.status(500).send(err);
-      return;
-    });
-});
-
-
-apiRouter.get(ServerUrlMap['Books.Unhide'], function(request, response) {
-  logger.debug(`get[${ServerUrlMap['Books.']}]`);
-  return Horace.unHideAllBooks();
-});
-
-
-
-appUse(ServerUrlMap.API, apiRouter);
+appUse(ServerUrlMap.API, apiRouter.getRouter(Horace));
